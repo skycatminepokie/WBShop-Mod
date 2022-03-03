8aefc45ad70bf2ee69069b1ae9e4c7999e666671
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-03-03 09:29:52 -0800

    Generated mod

---
+++ src/main/java/skycat/wbshop/WBShop.java
@@ -0,0 +1,10 @@
+package skycat.wbshop;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WBShop implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/skycat/wbshop/server/WBShopServer.java
@@ -0,0 +1,8 @@
+package skycat.wbshop.server;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.SERVER)
+public class WBShopServer {
+}
