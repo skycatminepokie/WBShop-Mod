2e7d7fcbca0bbd287655c14a212212e9c8708d09
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-03-18 11:29:23 -0700

    Economy framework mostly finished

--- src/main/java/skycat/wbshop/server/DonationManager.java
+++ src/main/java/skycat/wbshop/server/DonationManager.java
@@ -3,14 +3,18 @@ package skycat.wbshop.server;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.collection.DefaultedList;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Environment(EnvType.SERVER)
 public class DonationManager {
+    public static HashMap<Item, Integer> pointValues;
+
     public static void addHandler(ScreenHandler handler) {
         // TODO
         // This is how we managed to get the handler out of the method call in the main mod class. Hopefully we can manipulate it from here.
@@ -35,8 +39,9 @@ public class DonationManager {
         });
         */
     }
-    public static void donateScreenClosing(ScreenHandler handler, PlayerEntity player) { // OK so the only stacks we care about are 0-53. The rest are in the player's inventory
-        System.out.println("Closing handler with syncId " + handler.syncId); // Looks like we won't have to worry about managing these.
+    public static void donateScreenClosing(ScreenHandler handler, PlayerEntity player) {
+        // DEBUG:
+        // System.out.println("Closing handler with syncId " + handler.syncId); // Looks like we won't have to worry about managing syncIds. That's nice.
         DefaultedList<ItemStack> stacks = handler.getStacks();
         makeDonation(stacks.subList(0, 54), player); // Get the stacks in the double chest gui (stacks 0-53)
         /*
@@ -51,6 +56,19 @@ public class DonationManager {
     }
 
     public static void makeDonation(List<ItemStack> itemStacks, PlayerEntity player) {
-        itemStacks.iterator().forEachRemaining(itemStack -> System.out.println(itemStack.toString()));
+        itemStacks.iterator().forEachRemaining(itemStack -> getPointValue(itemStack));
+    }
+
+    public static int getPointValue(ItemStack itemStack) {
+        // DEBUG:
+        System.out.println(itemStack.getCount() + "x " + itemStack.getItem().toString());
+        return itemStack.getCount() * pointValues.get(itemStack.getItem());
+    }
+
+    public static void initializePointValues() {
+        // Load values from file
+        // DEBUG:
+        // Is an Item a representation of a kind of item, or is it an actual item that has gameplay meaning? I think it's the former.
+        // Can potentially use ItemStackArgumentType to make a command to update the values
     }
 }
