f48dc54decbf3e6f3d6f89ac4b5110d0635081a1
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-06-29 12:29:31 -0700

    Pay command implemented
    Fixes #7
    Build bumped

--- src/main/java/skycat/wbshop/commands/PayCommandHandler.java
+++ src/main/java/skycat/wbshop/commands/PayCommandHandler.java
@@ -1,9 +1,17 @@
 package skycat.wbshop.commands;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.brigadier.LiteralMessage;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import net.minecraft.command.argument.GameProfileArgumentType;
 import net.minecraft.server.command.ServerCommandSource;
 import skycat.wbshop.WBShopServer;
+import skycat.wbshop.server.EconomyManager;
+
+import java.util.Collection;
+import java.util.UUID;
 
 public class PayCommandHandler {
     public static int payCalled(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
@@ -13,6 +21,18 @@ public class PayCommandHandler {
     }
 
     public static int payCalledWithArgs(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        UUID sentFrom = context.getSource().getPlayer().getUuid();
+        Collection<GameProfile> sendTo = GameProfileArgumentType.getProfileArgument(context, "player");
+        int amount = context.getArgument("amount", int.class);
+
+        if (sendTo.size() != 1) {
+            throw new SimpleCommandExceptionType(new LiteralMessage("You can only pay one player at a time.")).create(); // TODO: Allow multiple recipients?
+        }
+        if (EconomyManager.getInstance().getBalance(sentFrom) >= amount) {
+            EconomyManager.getInstance().transferBalance(sentFrom, ((GameProfile) sendTo.toArray()[0]).getId(), amount);
+        } else {
+            throw new SimpleCommandExceptionType(new LiteralMessage("You don't have enought points for that!")).create();
+        }
 
         return 1;
     }
